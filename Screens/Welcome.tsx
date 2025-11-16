f3f0b524584a27792044900e842641084fec5452
Aakash <aakashbasnet.info@gmail.com>
2025-11-16 20:44:32 +0200

    some changes in the display

--- Screens/Welcome.tsx
+++ Screens/Welcome.tsx
@@ -1,27 +1,42 @@
-import React from 'react';
-import { View, Text, Image, StyleSheet, TouchableOpacity } from 'react-native';
-import Swiper from 'react-native-swiper';
-import UserForm from './Userform';
+import React from "react";
+import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
+import Swiper from "react-native-swiper";
+import UserForm from "./Userform";
 
 const Welcome: React.FC = ({ navigation }: any) => {
   return (
     <Swiper style={styles.wrapper} showsButtons loop>
       <View style={styles.slide}>
-        <Image source={require('../assets/welcome.jpg')} style={styles.image} />
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/welcome.jpg")}
+            style={styles.image}
+          />
+        </View>
         <Text style={styles.text}>Welcome to our App</Text>
       </View>
+
       <View style={styles.slide}>
-        <Image source={require('../assets/image2.jpg')} style={styles.image} />
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/image2.jpg")}
+            style={styles.image}
+          />
+        </View>
         <Text style={styles.text}>Enjoy our features</Text>
       </View>
+
       <View style={styles.slide}>
-        <Image source={require('../assets/image3.jpg')} style={styles.image} />
-        {/* <Text style={styles.text}>Stay Connected</Text> */}
-        <TouchableOpacity onPress={() => navigation.navigate('Userform')}>
-                  <Text style={{fontSize:20, color:"red" }}>Get Started</Text>
-                </TouchableOpacity>
+        <View style={styles.imageContainer}>
+          <Image
+            source={require("../assets/image3.jpg")}
+            style={styles.image}
+          />
+        </View>
+        <TouchableOpacity onPress={() => navigation.navigate("Userform")}>
+          <Text style={{ fontSize: 20, color: "red" }}>Get Started</Text>
+        </TouchableOpacity>
       </View>
-      
     </Swiper>
   );
 };
@@ -30,19 +45,31 @@ const styles = StyleSheet.create({
   wrapper: {},
   slide: {
     flex: 1,
-    justifyContent: 'center',
-    alignItems: 'center',
-    backgroundColor: '#9DD6EB',
+    justifyContent: "center",
+    alignItems: "center",
+    backgroundColor: "#9DD6EB",
+    paddingHorizontal: 20,
+    gap: 50,
   },
+
+  imageContainer: {
+    width: "80%",
+    aspectRatio: 1.2,
+    borderRadius: 20,
+    overflow: "hidden",
+    backgroundColor: "#fff",
+  },
+
   image: {
-    width: '80%',
-    height: '50%',
-    resizeMode: 'contain',
+    width: "100%",
+    height: "100%",
+    resizeMode: "cover",
   },
+
   text: {
-    color: '#fff',
+    color: "#fff",
     fontSize: 30,
-    fontWeight: 'bold',
+    fontWeight: "bold",
     marginTop: 20,
   },
 });
